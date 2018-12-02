ef134b7263a86f54e317b33f9a2849087c9cf9cc
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-12-02 13:35:20 +0545

    Logger, bodyparser and cookieparser

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@
  * app.js
  */
 
-import dotenv from 'dotenv';
 import path from 'path';
+
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
 import express from 'express';
+import logger from 'morgan';
 
 import routes from './routes';
 
@@ -19,6 +23,18 @@ const app = express();
 // view engine
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'pug');
+
+// logger
+app.use(logger('combined'));
+
+// body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+
+// cookie parser
+app.use(cookieParser());
 
 // serve statuc files from 'public'
 app.use(express.static(path.join(__dirname, './public')));
