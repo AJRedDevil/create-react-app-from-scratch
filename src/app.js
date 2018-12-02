3e81f1104d7d9265e9a42553d81b887e08a9ab88
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-12-02 13:24:37 +0545

    Handling 404 errors

--- src/app.js
+++ src/app.js
@@ -17,6 +17,13 @@ app.set('view engine', 'pug');
 // use routes
 app.use('/', routes);
 
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
 // error handlers
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
