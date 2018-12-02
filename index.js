4edc0cde2070ebb1402806f3d3c563bfd38c908e
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-12-02 11:44:35 +0545

    The server file

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
  * index.js
  */
 
-const logSomething = options => ({
-  ...options,
-  anotherOption: 'Hello!',
+import http from 'http';
+import express from 'express';
+
+// Express app setup
+const app = express();
+
+app.get('*', (req, res) => {
+  res.end('Hello Express');
 });
 
-const options = logSomething({ one: '1', two: '2' });
-
-console.log(options);
+const server = http.createServer(app);
+server.listen(3000);
+server.on('listening', () => {
+  console.log('Server is listening on port: 3000');
+});
